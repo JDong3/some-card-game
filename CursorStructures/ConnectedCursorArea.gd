1b7c5f9188a0c03f45c290d009f31417cfe6b780
JOSEPH Dong <joseph.dong@ibm.com>
2019-08-30 11:54:25 -0400

    need to complete plan for connected cursor areas and cursor hubs

---
+++ CursorStructures/ConnectedCursorArea.gd
@@ -0,0 +1,11 @@
+extends Node2D
+class_name ConnectedCursorArea
+
+var props
+
+func _init(props_):
+	"""
+	props_:
+
+	"""
+	props = props_
--- CursorStructures/CursorArea.gd
+++ CursorStructures/CursorArea.gd
@@ -1,16 +1,33 @@
 extends Node2D
 
+"""
+same as cursor area, but you need to provice your own combobulate function
+"""
+
 var props
+
+var cursor_position = 0
 
 func _init(props_):
 	"""
 	props:
-		cells:
+		cells: list of Cells that are contained in the CursorArea,
+		cursor_position?: specify an optional default cursor position
+		reverse=false: cursor_next decrements position and cursor_prev
+			increments position, this is for CursorAreas where the cells are
+			created from right to left
 	"""
 	props = props_
+
+	# if reverse is not specified use default false
+	if !props.has('reverse'):
+		props['reverse'] = false
+
+func add_cell(cell):
+	print('implement add_cell', self)
 
 func combobulate():
 	print('implement combobulate', self)
 
 func input(event):
-	pass
+	print('implement cursor_next/cursor_previous')
