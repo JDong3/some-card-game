e76dfb79da4c29446fd14e22753103efc7d27409
xxcandiez <josephdong1998@gmail.com>
2019-09-13 22:44:47 -0400

    ?

--- Global.gd
+++ Global.gd
@@ -4,5 +4,4 @@ var GAME_FOCUS_MANAGER = FocusManager.new()
 var START_FOCUS_MANAGER = FocusManager.new()
 var CHARACTER_FOCUS_MANAGER = FocusManager.new()
 
-var COMBAT_MANAGER = CombatManager.new()
 var TRANSACTION_INTERFACE = TransactionInterface.new()
