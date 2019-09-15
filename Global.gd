3f66201dbc641a812b25d6c750baf3541c3f468a
xxcandiez <josephdong1998@gmail.com>
2019-09-15 17:39:17 -0400

    need to put FightClub on hold

--- Global.gd
+++ Global.gd
@@ -4,4 +4,4 @@ var GAME_FOCUS_MANAGER = FocusManager.new()
 var START_FOCUS_MANAGER = FocusManager.new()
 var CHARACTER_FOCUS_MANAGER = FocusManager.new()
 
-var TRANSACTION_INTERFACE = TransactionInterface.new()
+var FIGHT_CLUB = FightClub.new()
