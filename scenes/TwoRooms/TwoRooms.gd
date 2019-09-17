2b2ed56f65ae0cb01fd2f357a55a1100782e67db
xxcandiez <josephdong1998@gmail.com>
2019-09-17 11:19:32 -0400

    working on room feel

---
+++ scenes/TwoRooms/TwoRooms.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var Room = load('res://scenes/TwoRooms/Room.tscn')
+
+func _ready():
+	var my_room = Room.instance()
+
+	add_child(my_room)
+
+	var my_room2 = Room.instance()
+	my_room2.position = Vector2(100, 100)
+	add_child(my_room2)
+
+	print(Room)
