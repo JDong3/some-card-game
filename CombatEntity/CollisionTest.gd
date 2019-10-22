14c3727e18ac4fa22e4866ffa06ff1d56551835e
xxcandiez <josephdong1998@gmail.com>
2019-10-21 23:47:43 -0400

    working on objs for interactables

--- CombatEntity/CollisionTest.gd
+++ CombatEntity/CollisionTest.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 func _ready():
 	var inter = DangerInt.new()
+	inter.position = Vector2(200, 200)
 	add_child(inter)
--- Interactable/DangerInt.gd
+++ Interactable/DangerInt.gd
@@ -4,5 +4,12 @@ class_name DangerInt
 func _init():
 	print(props)
 	props.sprite = PathSprite.new('res://assets/sprites/danger-button.png')
-	props.collision_shape = null
+	props.collision_shape = CollisionShape2D.new()
+
+	var shape = CapsuleShape2D.new()
+	shape.height = 0
+	shape.radius = 11
+	props.collision_shape.shape = shape
+
+
 	.init(props)
